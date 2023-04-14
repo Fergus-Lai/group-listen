9c15b9acfdcc4ffdf8d800c137c4979be1467608
Fergus Lai <ferguslai724@gmail.com>
2023-04-14 05:10:04 +0100

    Added Page Transistion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,24 @@ import "~/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 
 import { ToastContainer } from "react-toastify";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const router = useRouter();
   return (
     <ClerkProvider {...pageProps}>
       <ToastContainer theme="dark" />
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0, x: "-100vh" }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: "100vh" }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </ClerkProvider>
   );
 };
